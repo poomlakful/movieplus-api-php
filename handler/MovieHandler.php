61ea19f0cb896ec64ef5a49340da0eb38b623df5
Poom Tammawutikul <poomlak1994@hotmail.com>
2016-12-02 16:44:07 +0700

    can output with json obj in find all movie function

--- handler/MovieHandler.php
+++ handler/MovieHandler.php
@@ -5,6 +5,7 @@ require_once("/model/Movie.php");
 
 Class MovieHandler extends Rest {
 	public function find_all() {
+		$movie_list = array();
 		$conn = new Connect();
 		$sql = "SELECT * FROM movie";
 		$result = $conn->query($sql);		
@@ -12,7 +13,9 @@ Class MovieHandler extends Rest {
 		if ($result->num_rows > 0) {
 		    // output data of each row
 		    while($row = $result->fetch_assoc()) {
-		        echo "id = ".$row["id"]." : name = ".$row["name"];
+		        //echo "id = ".$row["id"]." : name = ".$row["name"];
+		        $item = new Movie($row["id"], $row["name"], $row["detail"], $row["poster"], $row["trailer"], $row["type"], $row["time"], $row["director"], $row["status"]);
+		    	array_push($movie_list,$item);
 		    }
 		} else {
 		    echo "0 results";
@@ -20,6 +23,8 @@ Class MovieHandler extends Rest {
 
 		$conn->close();
 
+		header('Content-Type: application/json');
+		echo json_encode($movie_list);
 		//echo "find_movie_all";
 	}
 
